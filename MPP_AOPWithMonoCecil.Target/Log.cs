fc3635e840c5fc65194a1ba9022bbdf804216a57
Vladimir <Vladimir@VLADIMIR-PC>
2016-10-27 21:29:56 +0300

    ADD implementation of Log

--- MPP_AOPWithMonoCecil.Target/Log.cs
+++ MPP_AOPWithMonoCecil.Target/Log.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,33 @@ namespace MPP_AOPWithMonoCecil.Target
 {
     public class Log : Attribute
     {
+        private static string fileName = "log.txt";
 
+        public void OnLoggerTargetMethodExecute(System.Reflection.MethodBase method, Dictionary<string, object> parameters, object result = null)
+        {
+            using(StreamWriter writer = new StreamWriter(new FileStream(fileName, FileMode.Append, FileAccess.Write)))
+            {
+                writer.Write("CLASS : {" + method.DeclaringType + "}. ");
+                writer.Write("METHOD: {" + method.Name + "}. ");
+                writer.Write("PARAMETERS: {");
+                if(parameters.Count > 0)
+                {
+                    foreach (KeyValuePair<string, object> keyValue in parameters)
+                    {
+                        writer.Write(keyValue.Key + " = " + keyValue.Value);
+                    }
+                }
+                else
+                {
+                    writer.Write("none");
+                }
+                writer.Write("} ");
+                if (result != null)
+                {
+                    writer.Write("and RETURNS {" + result + "}");
+                }
+                writer.WriteLine();
+            } 
+        }
     }
 }
--- MPP_AOPWithMonoCecil.Target/TargetAopClass.cs
+++ MPP_AOPWithMonoCecil.Target/TargetAopClass.cs
@@ -7,8 +7,24 @@ using System.Threading.Tasks;
 namespace MPP_AOPWithMonoCecil.Target
 {
     [Log]
-    class TargetAopClass
+    public class TargetAopClass
     {
+        private int parameter;
 
+        public TargetAopClass(int parameter)
+        {
+            this.parameter = parameter;
+        }
+
+        public void First()
+        {
+            Console.WriteLine("First Function");
+        }
+
+        public int Second(int parameter1, object parameter2)
+        {
+
+            return 10;
+        }
     }
 }
